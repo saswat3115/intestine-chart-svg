0e91986121d3951668f3e910e74cc3104803c692
saswat3115 <saswat3115@gmail.com>
2022-01-31 15:21:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-5jcnkk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-5jcnkk)
---
+++ script.js
@@ -0,0 +1,40 @@
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians),
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
+
+  var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+  var d = [
+    'M',
+    start.x,
+    start.y,
+    'A',
+    radius,
+    radius,
+    0,
+    largeArcFlag,
+    0,
+    end.x,
+    end.y,
+  ].join(' ');
+
+  return d;
+}
+
+window.onload = function () {
+  document
+    .getElementById('arc1')
+    .setAttribute('d', describeArc(150, 150, 100, 0, 270));
+  document
+    .getElementById('arc2')
+    .setAttribute('d', describeArc(150, 150, 100, 0, 200));
+};
